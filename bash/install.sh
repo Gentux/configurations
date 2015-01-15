affbc7a5df90470b21e7992a1fd52c5dbb676298
Romain Soufflet <romain@soufflet.io>
2015-01-15 11:54:07 +0100

    Add depency for previous patch

--- bash/install.sh
+++ bash/install.sh
@@ -3,6 +3,8 @@
 CURRENT_PATH=$(dirname $(readlink -e $0))
 HOME_DIR=${HOME}
 
+pip install slugify
+
 ln -s ${CURRENT_PATH}/bashrc ~/.bashrc
 ln -s ${CURRENT_PATH}/bash_aliases ~/.bash_aliases
 ln -s ${CURRENT_PATH}/dircolors ~/.dircolors
