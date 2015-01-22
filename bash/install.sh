b13a8531b02cbe21a6cddf716ebdbb8098cac089
Romain Soufflet <romain@soufflet.io>
2015-01-22 16:48:45 +0100

    Fix installation

--- bash/install.sh
+++ bash/install.sh
@@ -3,7 +3,7 @@
 CURRENT_PATH=$(dirname $(readlink -e $0))
 HOME_DIR=${HOME}
 
-pip install slugify
+sudo pip install slugify
 
 ln -s ${CURRENT_PATH}/bashrc ~/.bashrc
 ln -s ${CURRENT_PATH}/bash_aliases ~/.bash_aliases
--- bin/scripts/irssi-fnotify.sh
+++ bin/scripts/irssi-fnotify.sh
@@ -20,83 +20,83 @@
 # this requires your ~/.ssh/config LocalCommand to run this script with --start
 
 #
-#	globals
+#    globals
 #
-FILE='~/.irssi/fnotify'					# fnotify on server
-NOTIFY=`which notify-send`
-#NOTIFY='/bin/echo'					# useful for debugging!
-BEEP="`which paplay` /usr/share/sounds/gnome/default/alerts/drip.ogg"	# loud!
-#BEEP='/usr/bin/beep'
-COUNT="/tmp/`basename $0`.count"			# local in your /tmp/
-FIFO="/tmp/`basename $0`.fifo"				# local in your /tmp/
+
+FILE='~/.irssi/fnotify'             # fnotify on server
+NOTIFY=$(which notify-send)
+#NOTIFY='/bin/echo'                 # useful for debugging!
+BEEP="$(which paplay) /usr/share/sounds/gnome/default/alerts/drip.ogg"
+COUNT="/tmp/$(basename $0).count"   # local in your /tmp/
+FIFO="/tmp/$(basename $0).fifo"     # local in your /tmp/
 
 if [ "$1" == '--help' ]; then
-	# this method should be used from LocalCommand in ~/.ssh/config
-	echo "usage: ./"`basename $0`" [--start] <hostname> [--ppid <ppid>]"
-	exit 1
+    # this method should be used from LocalCommand in ~/.ssh/config
+    echo "usage: ./"$(basename $0)" [--start] <hostname> [--ppid <ppid>]"
+    exit 1
 fi
 
 if [ "$1" == '--start' ]; then
-	shift 1
-	SERVER=$1
-	# $PPID is pid of the main calling, ssh command
-	{ `$0 "$SERVER" --ppid "$PPID" &> /dev/null`; } &	# daemonize...
-	exit		# LocalCommand from ~/.ssh/config should not block
+    shift 1
+    SERVER=$1
+    # $PPID is pid of the main calling, ssh command
+    { $($0 "$SERVER" --ppid "$PPID" &> /dev/null); } &    # daemonize...
+    exit        # LocalCommand from ~/.ssh/config should not block
 fi
 
 # assume we're daemonized or running interactively...
 SERVER=$1
 shift 1
 if [ "$SERVER" == '' ]; then
-	echo "usage: ./"`basename $0`" <hostname> [--ppid <ppid>]"
-	exit 1
+    echo "usage: ./"$(basename $0)" <hostname> [--ppid <ppid>]"
+    exit 1
 fi
 
 WAIT=''
 if [ "$1" == '--ppid' ]; then
-	WAIT="$2"	# passed in $PPID
+    WAIT="$2"    # passed in $PPID
 fi
 
 # thanks to bahamas10 in #bash for giving me the idea to use a fifo...
 rm -f $FIFO
 mkfifo $FIFO
 ssh $SERVER "tail -F $FILE 2> /dev/null" > $FIFO &
-SSH_PID=$!	# save the pid
+SSH_PID=$!    # save the pid
 (
-	i=0
-	echo 0 > $COUNT
-	# TODO: should i use sed to get rid of invalid characters?
-	cat $FIFO | while
-	read network from msg; do
-		((i++))				# count messages
-		echo $i > $COUNT		# XXX: hack
-		xmsg=`/bin/echo "${msg}" | /usr/bin/recode ascii..html`	# escape
-		$NOTIFY -i gtk-dialog-info -t 300000 -- "${network}(${from})" "${xmsg}";
-		$BEEP
-	done
+    i=0
+    echo 0 > $COUNT
+    # TODO: should i use sed to get rid of invalid characters?
+    cat $FIFO | while
+    read network from msg; do
+        ((i++))                # count messages
+        echo $i > $COUNT        # XXX: hack
+        xmsg=$(/bin/echo "${msg}" | /usr/bin/recode ascii..html)    # escape
+        $NOTIFY -i gtk-dialog-info -t 300000 "${network}(${from})" "${xmsg}";
+        $BEEP
+    done
 ) &
 
 if [ "$WAIT" != '' ]; then
-	# TODO: sadly inotifywait /proc/$WAIT/ won't work for /proc, so we poll
-	while kill -0 2> /dev/null "$WAIT"; do
-		sleep 1s
-	done
+    # TODO: sadly inotifywait /proc/$WAIT/ won't work for /proc, so we poll
+    while kill -0 2> /dev/null "$WAIT"; do
+        sleep 1s
+    done
 fi
 
-kill $SSH_PID					# kill ssh process to clean up!
+kill $SSH_PID                    # kill ssh process to clean up!
 #kill -9 $SSH_PID
 if [ -p $FIFO ]; then
-	rm $FIFO
+    rm $FIFO
 fi
 
-C=`cat $COUNT`
+C=$(cat $COUNT)
 if [ -e $COUNT ]; then
-	rm $COUNT
+    rm $COUNT
 fi
 
 # truncate viewed messages...
 if [ $C -gt 0 ]; then
-	ssh $SERVER "tail -n +$((C+1)) $FILE | tee $FILE > /dev/null"
+    ssh $SERVER "tail -n +$((C+1)) $FILE | tee $FILE > /dev/null"
 fi
 
 exit
