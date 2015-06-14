31ef47ea36a95d24c43535d5a393a12ee806952c
Romain Soufflet <romain@soufflet.io>
2015-06-14 18:59:53 +0200

    And an installation script to rule them all.

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -xe
+
+
+CONFIGURATION_REPO_DIR=$(dirname $(readlink -e $0))
+
+for DIR in $(ls ${CONFIGURATION_REPO_DIR}); do
+    [ -d $DIR ] || continue;
+    [ -x ${DIR}/install.sh ] || continue;
+
+    ${DIR}/install.sh
+done
