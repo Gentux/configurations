1bff8c7de0e8fc6bf20f71b98725b0b0bb421293
Romain Soufflet <romain@soufflet.io>
2015-06-22 09:51:23 +0200

    Apply last modification to screen configuarations

--- screen/install.sh
+++ screen/install.sh
@@ -2,4 +2,6 @@
 
 CURRENT_PATH=$(dirname $(readlink -e $0))
 
-ln -s ${CURRENT_PATH}/screenrc ~/.screenrc
+source ${CURRENT_PATH}/../function.sh
+
+create_or_replace_symlink ${CURRENT_PATH}/screenrc ~/.screenrc
