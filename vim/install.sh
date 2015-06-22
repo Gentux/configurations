31928b7346ef8d6f56d1d095aa7d3e89c35274a8
Romain Soufflet <romain@soufflet.io>
2015-06-22 09:51:43 +0200

    Update VIM installation script

--- vim/install.sh
+++ vim/install.sh
@@ -4,12 +4,13 @@
 VIMDIR=$(readlink -e ~/.vim/)
 VIM_CONFIG_DIR=$(dirname $(readlink -e ${0}))
 
+source ${VIM_CONFIG_DIR}/../function.sh
+
 [ -d ${VIMDIR} ] || mkdir ${VIMDIR}
 [ -d ${VIMDIR}/ftplugin ] || mkdir ${VIMDIR}/ftplugin
 
-ln -s ${VIM_CONFIG_DIR}/vimrc   ~/.vimrc
-ln -s ${VIM_CONFIG_DIR}/gvimrc  ~/.gvimrc
-ln -s ${VIM_CONFIG_DIR}/ftplugin/dot.vim            ${VIMDIR}/ftplugin/
-ln -s ${VIM_CONFIG_DIR}/ftplugin/html.vim           ${VIMDIR}/ftplugin/
-ln -s ${VIM_CONFIG_DIR}/ftplugin/javascript.vim     ${VIMDIR}/ftplugin/
-ln -s ${VIM_CONFIG_DIR}/ftplugin/python.vim         ${VIMDIR}/ftplugin/
+create_or_replace_symlink ${VIM_CONFIG_DIR}/vimrc   ~/.vimrc
+create_or_replace_symlink ${VIM_CONFIG_DIR}/gvimrc  ~/.gvimrc
+for FTPLUGIN in $(ls ${VIM_CONFIG_DIR}/ftplugin/*.vim); do
+    create_or_replace_symlink ${FTPLUGIN} ${VIMDIR}/ftplugin/$(basename ${FTPLUGIN})
+done
